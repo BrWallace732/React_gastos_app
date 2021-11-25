c228f1b5256fe03b219ef6cf57f6863c9880d9eb
Bruno Wallace <Bruwallaceag@gmail.com>
2021-11-25 10:54:17 -0600

    agrupar gastos con misma fecha

--- src/components/ListaGastos.js
+++ src/components/ListaGastos.js
@@ -26,9 +26,28 @@ import {ReactComponent as IconoEditar} from './../images/pencil.svg'
 import {ReactComponent as IconoBorrar} from './../images/trash.svg'
 import { Link } from "react-router-dom";
 import Boton from "../elements/Boton";
+import { format, fromUnixTime } from "date-fns";
+import { es } from "date-fns/locale";
 
 const ListaGastos = () => {
     const [gastos] = useObtenerGastos()
+    const fechaFromat = (fecha) => {
+        return format(fromUnixTime(fecha), "dd 'de ' MMMM 'de ' yyyy", {locale: es})
+    }
+
+    const fechaEsIgual = (gastos, index, gasto) => {
+        if(index !== 0){
+            const fechaActual = fechaFromat(gasto.fecha)
+            const fechaGastoAnterior = fechaFromat(gastos[index-1].fecha)
+
+            if(fechaActual === fechaGastoAnterior){
+                return  true
+            } else{
+                return false
+            }
+        }
+    }
+
     return ( 
         <>
             <Helmet>
@@ -41,27 +60,31 @@ const ListaGastos = () => {
             </Header>
 
             <Lista>
-                {gastos.map((gasto)=>{
+                {gastos.map((gasto, index)=>{
                     return(
-                        <ElementoLista key={gasto.id}>
-                            <Categoria>
-                                <Iconos id={gasto.categoria} />
-                                {gasto.descripcion}
-                            </Categoria>
-                            <Descripcion>
-                                {gasto.descripcion}
-                            </Descripcion>
-                            <Valor>{convertirMoneda(gasto.cantidad)}</Valor>
-
-                            <ContenedorBotones>
-                                <BotonAccion as={Link} to={`/editar/${gasto.id}`} >
-                                    <IconoEditar />
-                                </BotonAccion>
-                                <BotonAccion >
-                                    <IconoBorrar />
-                                </BotonAccion>
-                            </ContenedorBotones>
-                        </ElementoLista>
+                        <div key={gasto.id}>
+                            {!fechaEsIgual(gastos, index, gasto) && 
+                                <Fecha>{fechaFromat(gasto.fecha)}</Fecha>
+                            } 
+                            <ElementoLista key={gasto.id}>
+                                <Categoria>
+                                    <Iconos id={gasto.categoria} />
+                                    {gasto.descripcion}
+                                </Categoria>
+                                <Descripcion>
+                                    {gasto.descripcion}
+                                </Descripcion>
+                                <Valor>{convertirMoneda(gasto.cantidad)}</Valor>
+                                <ContenedorBotones>
+                                    <BotonAccion as={Link} to={`/editar/${gasto.id}`} >
+                                        <IconoEditar />
+                                    </BotonAccion>
+                                    <BotonAccion >
+                                        <IconoBorrar />
+                                    </BotonAccion>
+                                </ContenedorBotones>
+                            </ElementoLista>
+                        </div>
                     )
                 })}
 
