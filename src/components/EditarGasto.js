55ec7b2cb758543ddbb043d7e7895c8111ee5307
Bruno Wallace <Bruwallaceag@gmail.com>
2021-12-09 17:15:33 -0600

    downgrade a la version "react-router-dom": "^5.1.2",

--- src/components/EditarGasto.js
+++ src/components/EditarGasto.js
@@ -4,13 +4,17 @@ import { Helmet } from "react-helmet";
 import BtnRegresar from "../elements/Regresar";
 import BarraTotalGastado from "./BarraTotal";
 import FormularioGasto from "./FormularioGasto";
-import { useParams } from "react-router-dom";
+ import { useParams } from "react-router-dom"; //"react-router-dom": "^5.1.2"
+// import { useSearchParams } from "react-router-dom"; "react-router-dom": "^5.1.3",
 import UseObtenerGasto from "../hooks/UseObtenerGaasto";
 
 
 
 const EditGasto = () => {
     const {id} = useParams()
+    // let [searchParams, setSearchParams] = useSearchParams(); "react-router-dom": "^5.1.3",
+
+    
     console.log(id)
     const [gasto] = UseObtenerGasto(id)
 
